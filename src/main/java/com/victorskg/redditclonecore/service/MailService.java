2368f067b1de4456bf7dfdaa0d0ef1474261c68c
João Victor <victorskg@outlook.com>
2020-05-28 16:49:04 -0300

    Envio de email async.

--- src/main/java/com/victorskg/redditclonecore/service/MailService.java
+++ src/main/java/com/victorskg/redditclonecore/service/MailService.java
@@ -8,6 +8,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import static java.lang.String.format;
@@ -21,6 +22,7 @@ public class MailService {
 
     private final MailBuilder mailBuilder;
 
+    @Async
     public void sendMail(NotificationEmail notificationEmail) {
         MimeMessagePreparator messagePrepare = mimeMessage -> {
             var helper = new MimeMessageHelper(mimeMessage);
